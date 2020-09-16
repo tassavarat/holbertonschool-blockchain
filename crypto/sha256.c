a5c33a071540cc8234f36fe5451121ab09560327
tassavarat <tassavarat@gmail.com>
2020-09-15 17:53:40 -0700

    Reworked for portability with older version of OpenSSL

--- crypto/sha256.c
+++ crypto/sha256.c
@@ -11,15 +11,7 @@
 uint8_t *sha256(int8_t const *s, size_t len,
 		uint8_t digest[SHA256_DIGEST_LENGTH])
 {
-	EVP_MD_CTX *mdctx;
-	unsigned int md_len;
-
 	if (!s || !digest)
 		return (NULL);
-	mdctx = EVP_MD_CTX_new();
-	EVP_DigestInit_ex(mdctx, EVP_get_digestbyname("SHA256"), NULL);
-	EVP_DigestUpdate(mdctx, s, len);
-	EVP_DigestFinal_ex(mdctx, digest, &md_len);
-	EVP_MD_CTX_free(mdctx);
-	return (digest);
+	return (SHA256((const unsigned char *)s, len, digest));
 }
