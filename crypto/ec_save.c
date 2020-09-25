90d45864c8d4a087e64a162ba140f0440a0ad418
tassavarat <tassavarat@gmail.com>
2020-09-24 19:41:05 -0700

    Adding comments

--- crypto/ec_save.c
+++ crypto/ec_save.c
@@ -1,8 +1,17 @@
 #include "hblk_crypto.h"
 
-int write_file(char *buf, char const *folder, char *fname, int type,
-		EC_KEY *key)
+/**
+ * write_file - write EC key to disk
+ * @folder: path to folder to save keys
+ * @fname: name of file to write
+ * @type: specifies whether to write private or public key
+ * @key: EC key pair to save
+ *
+ * Return: 1 on success, 0 on error
+ */
+int write_file(char const *folder, char *fname, int type, EC_KEY *key)
 {
+	char buf[BUFSIZ];
 	FILE *fp;
 
 	sprintf(buf, "%s/%s", folder, fname);
@@ -38,16 +47,15 @@ out:
  */
 int ec_save(EC_KEY *key, char const *folder)
 {
-	char buf[BUFSIZ];
 
 	if (!key || !folder)
 		return (0);
 	if (mkdir(folder, S_IRWXU | S_IRGRP | S_IXGRP | S_IROTH | S_IXOTH) ==
 			-1)
 		return (0);
-	if (!write_file(buf, folder, PRIV_NAME, PRIV_TYPE, key))
+	if (!write_file(folder, PRIV_NAME, PRIV_TYPE, key))
 		return (0);
-	if (!write_file(buf, folder, PUB_NAME, PUB_TYPE, key))
+	if (!write_file(folder, PUB_NAME, PUB_TYPE, key))
 		return (0);
 	return (1);
 }
