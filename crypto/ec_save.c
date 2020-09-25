346610cc8392468d64c1d79780a9fd7b5c81a1a7
tassavarat <tassavarat@gmail.com>
2020-09-24 19:46:13 -0700

    Validating checker

--- crypto/ec_save.c
+++ crypto/ec_save.c
@@ -1,62 +1,37 @@
 #include "hblk_crypto.h"
 
 /**
- * write_file - write EC key to disk
- * @folder: path to folder to save keys
- * @fname: name of file to write
- * @type: specifies whether to write private or public key
- * @key: EC key pair to save
- *
- * Return: 1 on success, 0 on error
- */
-int write_file(char const *folder, char *fname, int type, EC_KEY *key)
-{
-	char buf[BUFSIZ];
-	FILE *fp;
-
-	sprintf(buf, "%s/%s", folder, fname);
-	fp = fopen(buf, "w");
-	if (!fp)
-		return (0);
-	if (type == PRIV_TYPE)
-	{
-		if (!PEM_write_ECPrivateKey(fp, key, NULL, NULL, 0, NULL,
-					NULL))
-			goto out;
-	}
-	else if (type == PUB_TYPE)
-	{
-		if (!PEM_write_EC_PUBKEY(fp, key))
-			goto out;
-	}
-	else
-	{
-out:
-		fclose(fp);
-		return (0);
-	}
-	fclose(fp);
-	return (1);
-}
-
-/**
- * ec_save - save existing EC key pair to disk
- * @key: EC key pair to save
- * @folder: path to folder to save keys
- *
- * Return: 1 on success, 0 on error
- */
+* ec_save - save pub/private key pair into a file
+* @key: key pair
+* @folder: directory where to store the file
+* Return: 1 on success, 0 on failure
+*/
 int ec_save(EC_KEY *key, char const *folder)
 {
+	char file[512] = {0};
+	FILE *fp;
+	struct stat st = {0};
 
 	if (!key || !folder)
 		return (0);
-	if (mkdir(folder, S_IRWXU | S_IRGRP | S_IXGRP | S_IROTH | S_IXOTH) ==
-			-1)
+	if (stat(folder, &st) == -1)
+	{
+		if (mkdir(folder, 0700) == -1)
+			return (0);
+	}
+	sprintf(file, "%s/%s", folder, "key.pem");
+	fp = fopen(file, "w");
+	if (!fp)
 		return (0);
-	if (!write_file(folder, PRIV_NAME, PRIV_TYPE, key))
+	if (!PEM_write_ECPrivateKey(fp, key, NULL, NULL, 0, NULL, NULL))
 		return (0);
-	if (!write_file(folder, PUB_NAME, PUB_TYPE, key))
+	fclose(fp);
+	sprintf(file, "%s/%s", folder, "key_pub.pem");
+	fp = fopen(file, "w");
+	if (!fp)
 		return (0);
+	if (!PEM_write_EC_PUBKEY(fp, key))
+		return (0);
+	fclose(fp);
 	return (1);
 }
