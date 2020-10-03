0c37bae0a0a803047f6d58b7508973adce6c01a0
tassavarat <tassavarat@gmail.com>
2020-10-02 18:55:27 -0700

    Implementing ec_sign

--- crypto/ec_sign.c
+++ crypto/ec_sign.c
@@ -4,6 +4,7 @@
  * ec_sign - sign a set of bytes, using given private EC_KEY
  * @key: pointer to EC_KEY struct containing private key to perform the signing
  * @msg: pointer to characters to be signed
+ * @msglen: len of msg
  * @sig: address to store signature
  *
  * Return: pointer to signature buffer on success, NULL on error
@@ -11,5 +12,19 @@
 uint8_t *ec_sign(EC_KEY const *key, uint8_t const *msg, size_t msglen,
 		sig_t *sig)
 {
+	uint8_t md[SHA256_DIGEST_LENGTH];
 
+	if (!key || !msg || !sig)
+		return (NULL);
+	if (!EC_KEY_check_key(key))
+		return (NULL);
+	if (!SHA256(msg, msglen, md))
+		return (NULL);
+	sig->len = ECDSA_size(key);
+	if (!sig->len)
+		return (NULL);
+	if (!ECDSA_sign(EC_CURVE, md, SHA256_DIGEST_LENGTH, sig->sig,
+				(unsigned int *)&(sig->len), (EC_KEY *)key))
+		return (NULL);
+	return (sig->sig);
 }
