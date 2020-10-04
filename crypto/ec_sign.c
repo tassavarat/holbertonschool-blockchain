2bb539a1f0f8aa61d1f2ab4d97d4bf159af2b4a8
tassavarat <tassavarat@gmail.com>
2020-10-03 18:21:25 -0700

    Changing md to unsigned char

--- crypto/ec_sign.c
+++ crypto/ec_sign.c
@@ -12,7 +12,7 @@
 uint8_t *ec_sign(EC_KEY const *key, uint8_t const *msg, size_t msglen,
 		sig_t *sig)
 {
-	uint8_t md[SHA256_DIGEST_LENGTH];
+	unsigned char md[SHA256_DIGEST_LENGTH];
 
 	if (!key || !msg || !sig)
 		return (NULL);
